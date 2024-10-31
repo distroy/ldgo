1ea0a98ae7e751992265e36ce7b3ab74273bfca2
distroy <distroy@163.com>
2024-10-31 12:10:41 +0800

    chore(ldcmp): typedef

--- ldcmp/compare.go
+++ ldcmp/compare.go
@@ -13,8 +13,16 @@ type Integer interface {
 		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
 }
 
+type Float interface {
+	~float32 | ~float64
+}
+
 type Number interface {
-	Integer | ~float32 | ~float64
+	Integer | Float
+}
+
+type Complex interface {
+	~complex64 | ~complex128
 }
 
 type Comparable interface {
@@ -35,10 +43,10 @@ func CompareBool[T ~bool](a, b T) int { return cmp.CompareBool(a, b) }
 func CompareString[T ~string](a, b T) int { return cmp.CompareString(a, b) }
 func CompareBytes[T ~[]byte](a, b T) int  { return cmp.CompareBytes(a, b) }
 
-func CompareInteger[T Integer](a, b T) int                  { return CompareOrderable(a, b) }
-func CompareFloat[T ~float32 | ~float64](a, b T) int        { return cmp.CompareComparable(a, b) }
-func CompareNumber[T Number](a, b T) int                    { return CompareOrderable(a, b) }
-func CompareComplex[T ~complex64 | ~complex128](a, b T) int { return cmp.CompareComplex(a, b) }
+func CompareInteger[T Integer](a, b T) int { return cmp.CompareComparable(a, b) }
+func CompareFloat[T Float](a, b T) int     { return cmp.CompareComparable(a, b) }
+func CompareNumber[T Number](a, b T) int   { return cmp.CompareComparable(a, b) }
+func CompareComplex[T Complex](a, b T) int { return cmp.CompareComplex(a, b) }
 
 func CompareComparer[T Comparer[T]](a, b T) int  { return cmp.CompareComparer(a, b) }
 func CompareComparable[T Comparable](a, b T) int { return cmp.CompareComparable(a, b) }
