4f91597c79bb280119419d2a509480c013f5aa15
distroy <distroy@163.com>
2024-10-31 20:58:13 +0800

    chore(ldptr): merge file

--- ldptr/ptr_test.go
+++ ldptr/ptr_test.go
@@ -18,3 +18,14 @@ func TestGet(t *testing.T) {
 		convey.So(Get[time.Duration](New[time.Duration](100), time.Duration(0)), convey.ShouldEqual, time.Duration(100))
 	})
 }
+
+func TestNewByPtr(t *testing.T) {
+	convey.Convey(t.Name(), t, func() {
+		convey.So(NewByPtr[time.Duration](nil), convey.ShouldBeNil)
+		convey.So(NewByPtr[time.Duration](nil, 0), convey.ShouldResemble, New[time.Duration](0))
+		convey.So(NewByPtr[time.Duration](nil, 1), convey.ShouldResemble, New[time.Duration](1))
+		convey.So(NewByPtr[time.Duration](New[time.Duration](1)), convey.ShouldResemble, New[time.Duration](1))
+		convey.So(NewByPtr[time.Duration](New[time.Duration](100)), convey.ShouldResemble, New[time.Duration](100))
+		convey.So(NewByPtr[time.Duration](New[time.Duration](-100)), convey.ShouldResemble, New[time.Duration](-100))
+	})
+}
